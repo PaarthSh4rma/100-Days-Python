48b4e8d26a4aa2a5f1d5120c2287e49fab6e7437
PaarthSh4rma <paarth.sharma.3110@gmail.com>
2023-07-31 01:25:22 +1000

    Day 1 - Band Name Generator

---
+++ Day_1_Band_Name_Generator.py
@@ -0,0 +1,9 @@
+"""
+100 Days of Code - Python
+Day 1 - Band Name Generator
+"""
+
+print("Welcome to the Band Name Generator.")
+town = input("What's the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your band's name could be " + town + " " + pet)
